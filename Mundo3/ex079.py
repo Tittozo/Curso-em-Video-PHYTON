415e8dad93896ff54959e11eb401972ed685a3b8
Mateus Antunes <mateusetec424@gmail.com>
2026-02-16 17:25:12 -0300

    Update ex079.py

--- Mundo3/ex079.py
+++ Mundo3/ex079.py
@@ -1,3 +1,20 @@
-#DESAFIO 079 DO CURSO EM VÍDEO
+#DESAFIO 79 DO CURSO EM VÍDEO
+#Valores unicos em uma lista
+
+valores = []
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in valores:
+        valores.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+
+    r = str(input('Quer continuar? [S/N]').strip().upper())
+    if r == 'N':
+        break
+print('-='*30)
+print(f'Você digitou os valores {valores}')
+
 
 
